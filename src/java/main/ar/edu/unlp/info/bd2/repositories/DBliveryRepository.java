7eb384a48f0311f9ca247ddabb71bc29b7be5760
elgrecoLucas <lucasgustavo8830@gmail.com>
2020-03-28 15:37:30 -0300

    Agrego el atributo sessionFactory y los import correspondientes a la clase DBliveryRepository

--- src/java/main/ar/edu/unlp/info/bd2/repositories/DBliveryRepository.java
+++ src/java/main/ar/edu/unlp/info/bd2/repositories/DBliveryRepository.java
@@ -1,5 +1,19 @@
 package ar.edu.unlp.info.bd2.repositories;
 
+import ar.edu.unlp.info.bd2.model.*;
+import org.hibernate.*;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 public class DBliveryRepository {
+	
+	SessionFactory sessionFactory;
 
 }
