195438943cb656a41c98c55e01d749ce65820ac1
elgrecoLucas <lucasgustavo8830@gmail.com>
2020-03-28 21:23:19 -0300

    clase User

---
+++ src/java/main/ar/edu/unlp/info/bd2/model/User.java
@@ -0,0 +1,13 @@
+package ar.edu.unlp.info.bd2.model;
+
+import javax.persistence.*; //Utilizo la persistencia de JPA no hibernate...
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+@Entity
+@Table(name="users")
+public class User {
+
+}
