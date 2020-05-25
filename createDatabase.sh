423accbf6fc9ccd5c9e5826489dd2d70aaf4324a
Lucas <lucasgustavo8830@gmail.com>
2020-05-25 10:44:47 -0300

    ff

--- createDatabase.sh
+++
@@ -1,6 +0,0 @@
-mysql -u root -e "drop database if exists bd2_grupo3;
-                  create database bd2_grupo3;
-                  CREATE USER IF NOT EXISTS 'bd2'@'localhost' IDENTIFIED BY 'bd2';
-                  GRANT ALL PRIVILEGES ON bd2_grupo3.* TO 'bd2'@'localhost';
-                  FLUSH PRIVILEGES;
-                  "
